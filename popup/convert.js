f6a60f0ccd7faf304ffb591a22dce9f6081c12fb
Tanmay Mohapatra <m.tanmay17@gmail.com>
2022-08-28 18:08:56 +0530

    Add functionalities

---
+++ popup/convert.js
@@ -0,0 +1,26 @@
+let convertButton = document.getElementById('convertList');
+convertButton.addEventListener('click', convertFunction);
+
+
+function convertFunction() {
+    let inputText = document.getElementById('listInput').value;
+
+    const prefixRegex = new RegExp(/(^\S+\s+)|^/, 'gmi');
+    const suffixRegex = new RegExp(/(\.\n)|\n|\.|$/, 'gmi');
+
+    let prefixResult = inputText.replace(prefixRegex, '\'');
+    let suffixResult = prefixResult.replace(suffixRegex, '\',\n');
+
+    document.getElementById('stringsOutput').value = suffixResult;
+}
+
+let copyButton = document.getElementById('copyButton');
+copyButton.addEventListener('click', copyFunction);
+
+function copyFunction() {
+    let copyText = document.getElementById('stringsOutput');
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+
+    copyButton.innerHTML = 'Copied!';
+}
