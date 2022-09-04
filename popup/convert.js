92311115c12f680e92141ae9a89f793bfb506819
Tanmay Mohapatra <m.tanmay17@gmail.com>
2022-09-04 11:10:29 +0530

    Add tabs and other functions

--- popup/convert.js
+++ popup/convert.js
@@ -1,3 +1,36 @@
+//Tabs functionality taken from https://codepen.io/aburkalo/pen/vYRdOBV
+const tabs = document.querySelectorAll(".tab");
+const tabContent = document.querySelectorAll(".tab-content");
+
+let tabNo = 0;
+let contentNo = 0;
+
+tabs.forEach((tab) => {
+  tab.dataset.id = tabNo;
+  tabNo++;
+  tab.addEventListener("click", function () {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+      tab.classList.add("non-active");
+    });
+    this.classList.remove("non-active");
+    this.classList.add("active");
+    tabContent.forEach((content) => {
+      content.classList.add("hidden");
+      if (content.dataset.id === tab.dataset.id) {
+          content.classList.remove("hidden");
+      }
+    });
+  });
+});
+
+tabContent.forEach((content) => {
+  content.dataset.id = contentNo;
+  contentNo++;
+});
+
+
+// TestSteps Conversion
 let convertButton = document.getElementById('convertList');
 convertButton.addEventListener('click', convertFunction);
 
@@ -24,3 +57,4 @@ function copyFunction() {
 
     copyButton.innerHTML = 'Copied!';
 }
+
